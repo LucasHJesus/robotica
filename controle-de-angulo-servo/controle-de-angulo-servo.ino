cbd5ce19a04c7c04c3453c1e18298888834ae21c
LucasHJesus <lukaslima641@gmail.com>
2023-08-30 16:42:17 -0300

    controlador de angulo do servo

---
+++ controle-de-angulo-servo/controle-de-angulo-servo.ino
@@ -0,0 +1,44 @@
+#include <Servo.h>
+#define pinoServo 6
+Servo servo;
+int value;
+
+void writeValue(int value)
+{
+      Serial.print(" | ");
+      Serial.println(value);
+      Serial.print("Servo posicionado em ");
+      Serial.print(value);
+      Serial.println(" graus");
+
+}
+
+void setup() 
+{
+  servo.attach(pinoServo);
+  Serial.begin(9600);
+  while(!Serial);
+
+  delay(1000);
+  Serial.println("Digite o grau de movimento e tecle enter...");
+}
+
+void loop() 
+{
+  if (Serial.available())
+  {
+    value = Serial.parseInt();
+    if( value >= 0 && value <= 180)
+    {
+      writeValue(value);
+      servo.write(value);
+      delay(5000);
+    }
+    else if(value == 0)
+    {
+      writeValue(10);
+      servo.write(10);
+    }
+    else Serial.println("Insira um angulo valido");
+  }
+}
